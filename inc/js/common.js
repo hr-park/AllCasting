30cec985d1e02f917d86d8aebc14923637ed1c60
hr-park <ever-iver@hanmail.net>
2024-05-14 19:52:39 +0900

    t

--- inc/js/common.js
+++ inc/js/common.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    //seletbox
     let x, i, j, l, ll, selElmnt, a, b, c;
     x = document.getElementsByClassName("sel_box");
     l = x.length;
@@ -29,6 +30,10 @@ document.addEventListener("DOMContentLoaded", () => {
                             y[k].classList.remove("same-as-selected");
                         }
                         this.classList.add("same-as-selected");
+
+                        // 값을 선택한 후에 "has_value" 클래스를 추가
+                        h.classList.add('has_value');
+
                         break;
                     }
                 }
@@ -45,6 +50,8 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 
+    
+    // 모든 선택 상자 닫는 함수
     function closeAllSelect(elmnt) {
         let x, y, xl, yl, arrNo = [];
         x = document.getElementsByClassName("select-items");
@@ -68,97 +75,53 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("click", closeAllSelect);
 
     //popup
-    $('.btn-example').click(function(){
-        const $href = $(this).attr('href');
-        layer_popup($href);
-    });
-
-    function layer_popup(el){
-        const $el = $(el);    // 레이어의 id를 $el 변수에 저장
-        const isDim = $el.prev().hasClass('dim'); // dimmed 레이어를 감지하기 위한 boolean 변수
-
-        isDim ? $('.dim_layer').fadeIn() : $el.fadeIn();
-
-        const $elWidth = $el.outerWidth(),
-            $elHeight = $el.outerHeight(),
-            docWidth = $(document).width(),
-            docHeight = $(document).height();
-
-        // 화면의 중앙에 레이어를 띄웁니다.
+    const layer_popup = (el) => {
+        const $el = document.querySelector(el);    // 레이어의 id를 $el 변수에 저장
+        const isDim = $el.previousElementSibling.classList.contains('dim'); // dimmed 레이어를 감지하기 위한 boolean 변수
+        console.log(el);
+        isDim ? document.querySelector('.dim_layer').style.display = 'block' : $el.style.display = 'block';
+    
+        /*const $elWidth = $el.offsetWidth,
+            $elHeight = $el.offsetHeight,
+            docWidth = document.documentElement.clientWidth,
+            docHeight = document.documentElement.clientHeight;
+    
         if ($elHeight < docHeight || $elWidth < docWidth) {
-            $el.css({
-                marginTop: -$elHeight /2,
-                marginLeft: -$elWidth/2
-            });
+            $el.style.marginTop = `${-$elHeight / 2}px`;
+            $el.style.marginLeft = `${-$elWidth / 2}px`;
         } else {
-            $el.css({top: 0, left: 0});
-        }
+            $el.style.top = '0';
+            $el.style.left = '0';
+        }*/
 
-        $el.find('a.btn_pop_close.active').click(function(){
-            isDim ? $('.dim_layer').fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힙니다.
+        //$el.querySelector('a.btn_pop_close.active').addEventListener('click', () => {
+            $el.querySelector('a.btn_pop_close').addEventListener('click', () => {
+            //isDim ? document.querySelector('.dim_layer').style.display = 'none' : $el.style.display = 'none';
+            //document.querySelector('.dim_layer').style.display = 'none';
+            $el.style.display = 'none';
             return false;
         });
-
-        $('.layer .dim').click(function(){
-            $('.dim_layer').fadeOut();
+    
+        document.querySelector('.dim_layer .dim').addEventListener('click', () => {
+            document.querySelector('.dim_layer').style.display = 'none';
             return false;
         });
-    }
+    };
 
-    function chkNumber(target, num){
-        if(num > 0){
-            target.value = target.value.replace(/[^0-9]/g, '').replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`).replace(/(\-{1,2})$/g, "");
-        } else {
-            target.value = target.value.replace(/[^0-9]/g, '');
-        }
-    }
-
-    $(document).ready(() => {
-        const tag = {};
-        let counter = 0;
-    
-        const addTag = (value) => {
-            tag[counter] = value;
-            counter++;
-        };
-    
-        const marginTag = () => Object.values(tag).filter(word => word !== "");
-    
-        $("#tag-form").on("submit", (e) => {
-            const value = marginTag(); // 배열로 반환
-            $("#rdTag").val(value); 
-            $(e.target).submit();
-        });
-    
-        $("#tag").on("keypress", (e) => {
-            const self = $(e.target);
-    
-            if (e.key === "Enter" || e.keyCode === 32) {
-                const tagValue = self.val(); 
-    
-                // 해시태그 값이 없으면 실행X
-                if (tagValue !== "") {
-                    // 중복 태그 확인
-                    const result = Object.values(tag).filter(word => word === tagValue);
-                    
-                    // 중복 태그가 없으면 실행
-                    if (result.length === 0) { 
-                        $("#tag_list").append(`<li class='tag_item'>${tagValue}<span class='btn_del' idx='${counter}'>태그삭제</span></li>`);
-                        addTag(tagValue);
-                        self.val("");
-                    } else {
-                        alert("특기가 중복됩니다.");
-                    }
-                }
-                e.preventDefault(); // 스페이스바 시 빈 공간이 생기지 않도록 방지
-            }
-        });
-    
-        // 인덱스 검사 후 삭제
-        $(document).on("click", ".btn_del", (e) => {
-            const index = $(e.target).attr("idx");
-            tag[index] = "";
-            $(e.target).parent().remove();
+    document.querySelectorAll('.btn-example').forEach(button => {
+        button.addEventListener('click', function() {
+            const href = this.getAttribute('href');
+            layer_popup(href);
         });
     });
+
+   
 });
+
+function chkNumber(target, num){
+    if (num > 0) {
+        target.value = target.value.replace(/[^0-9]/g, '').replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, '$1-$2-$3').replace(/-{1,2}$/, '');
+    } else {
+        target.value = target.value.replace(/[^0-9]/g, '');
+    }
+};
